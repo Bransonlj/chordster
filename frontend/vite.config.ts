a1208c53388bc8039f7a2359545f178ec6316df5
Bransonlj <Bransonlj@gmail.com>
2023-08-25 17:42:40 +0800

    Dockerize app

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,      
-        ws: true,
-      }
+export default ({ mode }) => {
+
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_SERVER_NAME && process.env.VITE_SERVER_PORT
+          ? `http://${process.env.VITE_SERVER_NAME}:${process.env.VITE_SERVER_PORT}`
+          : 'http://localhost:5000',
+          changeOrigin: true,
+          secure: false,      
+          ws: true,
+        }
+      },
+      host: true,
+      port: parseInt(process.env.VITE_PORT) || 8080,
     },
-    host: true, // Here
-    port: 8080,
-  },
-})
+  })
+}
